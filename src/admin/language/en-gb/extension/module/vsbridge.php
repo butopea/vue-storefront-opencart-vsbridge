d6cdc9c09316789ef569111d18e02dd01c107e2b
Mohammad Tomaraei <themreza@gmail.com>
2020-01-03 17:44:03 +0100

    added missing language

--- src/admin/language/en-gb/extension/module/vsbridge.php
+++ src/admin/language/en-gb/extension/module/vsbridge.php
@@ -10,12 +10,14 @@ $_['text_edit']        = 'Edit VS Bridge Module';
 // Entry
 $_['entry_status']     = 'Status';
 $_['entry_secret_key'] = 'Secret Key';
+$_['entry_endpoint_statuses'] = 'Endpoint Statuses';
 
 // Button
 $_['button_generate_secret_key'] = 'Generate Secret Key';
 
 // Info
 $_['info_secret_key'] = 'Must be at least 12 characters in length, contain numbers, upper and lowercase letters, and one of the following special characters: *&!@%^#$';
+$_['info_endpoint_statuses'] = 'Each endpoint can be enabled or disabled depending on your needs (for example, if you want to use an external checkout).';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify VS Bridge module!';
